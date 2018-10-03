b371858b1ce5a8078fb2a053d636fa9f87b49d5f
Rocha <jrocha@proforma.com>
2018-10-03 15:36:32 -0400

    DataDisplaying. 3 pages routed/working. Adding map next and evaluating working data.

--- server/Startup.cs
+++ server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,13 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+        services.AddCors();
+        
+            services
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<TideTrackerContext>(opt => 
+                    opt.UseNpgsql("server=localhost;username=postgres;password=warrior;database=TideTracker"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,6 +49,12 @@ namespace server
             }
 
             app.UseHttpsRedirection();
+             app.UseCors(builder =>
+          builder
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
             app.UseMvc();
         }
     }
